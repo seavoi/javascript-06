367a9a881ac54dfca20fc8067ae88046094d5754
Sean V <s.voivoi@gmail.com>
2019-06-27 20:43:28 -0500

    WIP

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Sean Voigts
 
 /* node app.js */
 https://teamtreehouse.com/projects/static-nodejs-and-express-site
+
+Error with project images on project page
+Create error stuff!
+Technologies
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+/* Requirements */
 const express = require('express');
-const { data } = require('data.json');
-
 const app = express();
+const { projects } = require('./data.json');
 
+/* Middleware */
 app.use('/static', express.static('public'));
-
 app.set('view engine', 'pug');
 
+/* Routes */
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', {projects: projects});
 });
 
 app.get('/about', (req, res) => {
@@ -16,9 +17,16 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/project', (req, res) => {
-	res.render('project');
+	res.redirect( "/" );
 });
 
+app.get("/project/:id", (req, res, next) => {
+    const id = req.params.id;
+    res.locals.project = projects[id];
+    res.render("project");
+});
+
+/* Local Host Starter */
 app.listen(3000, () => {
-  console.log('The application is running on localhost:3000!')
+  console.log('You should take a gander at localhost:3000, pal.')
 });
